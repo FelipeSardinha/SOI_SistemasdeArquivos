b95b31efffc783acbbf82d7961cfde34ed0a29f1
biabi <biabi@DESKTOP-D5R0ONJ>
2020-11-19 18:49:48 -0300

    SOI_SistemasdeArquivos

---
+++ src/view/Principal.java
@@ -0,0 +1,30 @@
+package view;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+import controller.FifaController;
+import controller.FifaControllerInterface;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		String caminho = "C:\\Users\\biabi\\Downloads\\home\\leandro\\Downloads\\ExercicioSistemaArquivos";
+		String nome = "data.csv";
+		FifaControllerInterface ififa = new FifaController();
+		Stack<String> jogadores = new Stack<String>();
+		List<String> revelacoes = new LinkedList<String>();
+		
+		try {
+			jogadores = ififa.empilhaBrasileiros(caminho, nome);
+			ififa.desempilhaBonsBrasileiros(jogadores);
+			revelacoes = ififa.listaRevelacoes(caminho, nome);
+			ififa.buscaListaJogadoresRevelacao(revelacoes);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}		
+}
